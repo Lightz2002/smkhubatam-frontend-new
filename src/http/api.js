c2992320e22373cc18ce1cc8bc24be4b90c489cd
Ryan kenidy <kenidyryan@gmail.com>
2023-04-12 08:44:22 +0700

    add some apis and queries

--- src/http/api.js
+++ src/http/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { redirect } from "react-router-dom";
 
 function getToken() {
   return localStorage.getItem("token")
@@ -6,12 +7,12 @@ function getToken() {
     : null;
 }
 
-const baseApi = axios.create({
+const client = axios.create({
   baseURL: "http://localhost:3030",
 });
 
 export const profile = async () => {
-  let response = await baseApi.get("profile", {
+  let response = await client.get("profile", {
     headers: {
       Authorization: getToken(),
     },
@@ -21,13 +22,13 @@ export const profile = async () => {
 };
 
 export const login = async credentials => {
-  let response = await baseApi.post("auth/login", { ...credentials });
+  let response = await client.post("auth/login", { ...credentials });
 
   return response;
 };
 
-export const getRoleMenu = async roleId => {
-  let response = await baseApi.get(`/roles/rolemenu?role=${roleId}`, {
+export const getUser = async userId => {
+  let response = await client.get(`/users/${userId}`, {
     headers: {
       Authorization: getToken(),
     },
@@ -36,10 +37,224 @@ export const getRoleMenu = async roleId => {
 };
 
 export const getUsers = async () => {
-  let response = await baseApi.get(`/users`, {
+  let response = await client.get(`/users`, {
     headers: {
       Authorization: getToken(),
     },
   });
+  return response;
+};
+
+export const createUser = async credentials => {
+  try {
+    let response = await client.post(
+      "users",
+      { ...credentials },
+      {
+        headers: {
+          Authorization: getToken(),
+        },
+      }
+    );
+    return response;
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const updateUser = async (userId, credentials) => {
+  let response = await client.put(
+    `users/${userId}`,
+    { ...credentials },
+    {
+      headers: {
+        Authorization: getToken(),
+      },
+    }
+  );
+
+  return response;
+};
+
+/* Role */
+
+export const getRoles = async () => {
+  let response = await client.get(`/roles`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const getRoleMenu = async roleId => {
+  let response = await client.get(`/roles/rolemenu?role=${roleId}`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+/* Location */
+export const getLocations = async () => {
+  let response = await client.get(`/locations`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const getLocation = async locationId => {
+  let response = await client.get(`/locations/${locationId}`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const createLocation = async credentials => {
+  let response = await client.post(
+    `/locations`,
+    { ...credentials },
+    {
+      headers: {
+        Authorization: getToken(),
+      },
+    }
+  );
+  return response;
+};
+
+export const updateLocation = async (locationId, credentials) => {
+  let response = await client.put(
+    `/locations/${locationId}`,
+    { ...credentials },
+    {
+      headers: {
+        Authorization: getToken(),
+      },
+    }
+  );
+  return response;
+};
+
+/* School Class */
+export const getSchoolClasses = async () => {
+  let response = await client.get(`/schoolclasses`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const getSchoolClass = async schoolClassId => {
+  let response = await client.get(`/schoolclasses/${schoolClassId}`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const createSchoolClass = async credentials => {
+  let response = await client.post(
+    `/schoolclasses`,
+    { ...credentials },
+    {
+      headers: {
+        Authorization: getToken(),
+      },
+    }
+  );
+  return response;
+};
+
+export const updateSchoolClass = async (schoolClassId, credentials) => {
+  let response = await client.put(
+    `/schoolclasses/${schoolClassId}`,
+    { ...credentials },
+    {
+      headers: {
+        Authorization: getToken(),
+      },
+    }
+  );
+  return response;
+};
+
+/* Majors */
+export const getMajors = async () => {
+  let response = await client.get(`/majors`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const getMajor = async () => {
+  let response = await client.get(`/majors`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const createMajors = async () => {
+  let response = await client.post(`/majors`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+/* Internship */
+export const getInternships = async () => {
+  let response = await client.get(`/internships`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const getInternship = async internshipId => {
+  let response = await client.get(`/internships/${internshipId}`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
+  return response;
+};
+
+export const createInternship = async credentials => {
+  let response = await client.post(
+    `/internships`,
+    { ...credentials },
+    {
+      headers: {
+        Authorization: getToken(),
+      },
+    }
+  );
+  return response;
+};
+
+export const updateInternship = async (internshipId, credentials) => {
+  let response = await client.put(
+    `/internships/${internshipId}`,
+    { ...credentials },
+    {
+      headers: {
+        Authorization: getToken(),
+      },
+    }
+  );
   return response;
 };
--- src/http/queries.js
+++ src/http/queries.js
@@ -1,25 +1,75 @@
-import { getRoleMenu, getUsers, profile } from "./api";
+import {
+  getInternship,
+  getInternships,
+  getLocation,
+  getLocations,
+  getMajors,
+  getRoleMenu,
+  getRoles,
+  getSchoolClass,
+  getSchoolClasses,
+  getUser,
+  getUsers,
+  profile,
+} from "./api";
 
 export const profileQuery = () => ({
   queryKey: ["user"],
-  queryFn: async () => {
-    const user = await profile();
-    return user;
-  },
+  queryFn: () => profile(),
 });
 
 export const getRoleMenuQuery = roleId => ({
   queryKey: ["menu"],
-  queryFn: async () => {
-    const menu = await getRoleMenu(roleId);
-    return menu;
-  },
+  queryFn: () => getRoleMenu(roleId),
+  enabled: !!roleId,
 });
 
 export const getUsersQuery = () => ({
   queryKey: ["users"],
-  queryFn: async () => {
-    const users = await getUsers();
-    return users;
-  },
+  queryFn: () => getUsers(),
+});
+
+export const getUserQuery = userId => ({
+  queryKey: ["user", userId],
+  queryFn: () => getUser(userId),
+});
+
+export const getRolesQuery = () => ({
+  queryKey: ["roles"],
+  queryFn: () => getRoles(),
+});
+
+export const getLocationsQuery = () => ({
+  queryKey: ["locations"],
+  queryFn: () => getLocations(),
+});
+
+export const getLocationQuery = locationId => ({
+  queryKey: ["location", locationId],
+  queryFn: () => getLocation(locationId),
+});
+
+export const getSchoolClassesQuery = () => ({
+  queryKey: ["schoolclasses"],
+  queryFn: () => getSchoolClasses(),
+});
+
+export const getSchoolClassQuery = schoolClassId => ({
+  queryKey: ["schoolclass", schoolClassId],
+  queryFn: () => getSchoolClass(schoolClassId),
+});
+
+export const getMajorsQuery = () => ({
+  queryKey: ["majors"],
+  queryFn: () => getMajors(),
+});
+
+export const getInternshipsQuery = () => ({
+  queryKey: ["internships"],
+  queryFn: () => getInternships(),
+});
+
+export const getInternshipQuery = internshipId => ({
+  queryKey: ["internship", internshipId],
+  queryFn: () => getInternship(internshipId),
 });
