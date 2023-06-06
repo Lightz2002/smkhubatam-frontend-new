f83459c84b9674d4d2fafba3a71df81b486d70e1
Ryan kenidy <kenidyryan@gmail.com>
2023-06-06 16:19:20 +0700

    remove comment

--- src/components/Dashboard/DashboardSidebarMenu.jsx
+++ src/components/Dashboard/DashboardSidebarMenu.jsx
@@ -4,20 +4,6 @@ import DashboardSidebarMenuItem from "./DashboardSidebarMenuItem";
 
 const DashboardSidebarMenu = ({ menus, loading }) => {
   return (
-    // <Stack
-    //   sx={{
-    //     flexGrow: 1,
-    //     py: 3,
-    //     display: "flex",
-    //     flex: 1,
-    //   }}
-    //   spacing={2}
-    // >
-    //   {menus.map(menu => (
-    //     <DashboardSidebarMenuItem key={menu.Id} menu={menu} />
-    //   ))}
-    // </Stack>
-
     <List>
       {loading && <div>Loading...</div>}
       {menus.map(menu => (
